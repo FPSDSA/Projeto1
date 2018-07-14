08d50ce972c06d46848e6b0701269a610de260be
SANTOS Flavio <flavio.santos@sodexo.com>
2018-07-14 14:42:41 -0300

    arquivo de pre processamento

---
+++ Pre-processamento/Pre-proc1.R
@@ -0,0 +1,19 @@
+dataset <- read.csv("vendas.csv")
+str(dataset)
+
+
+plot(dataset$Valor,dataset$Custo)
+
+
+
+
+
+library(ggplot2)
+
+c
+
+library(ggplot2)
+
+detach("package:ggplot2", unload=TRUE)
+
+meltedDComp 
